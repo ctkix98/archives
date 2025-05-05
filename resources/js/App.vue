e24e9b780d838b88f81e0b0cfc208afd9bc70fdd
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-05 14:26:18 +0200

    Init project with Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Bienvenue dans l’application d’archivage 📚</h1>
+  </template>
+  
+  <script>
+  export default {}
+  </script>
+  
---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Archivage de cours</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
